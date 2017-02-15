a11e0042b4764cf6264b5ca9a23abc478e2871be
michael <mihai@mandrescu.co>
2017-02-15 14:19:46 +0000

    Moved and updated scaffolding file.

--- generate_scaffolding.sh
+++ generate_scaffolding.sh
@@ -21,17 +21,41 @@ echo "Now generating directories."
 name=$1
 name_size=${#name}
 
-echo "Name: $name"
-echo "Name size: $name_size"
-
 if [ "${name_size:-0}" -ge 3 ]; then
-    mkdir $name
+    echo "Will use provided name: $name"
 else
     echo "Name $1 is either empty or too short. Defaulting to WebApp."
-    mkdir WebApp
+    name="WebApp"
 fi
 
+mkdir $name
 mkdir config
 mkdir instance
+
+echo "Now generating __init__ and settings files."
+
+touch $name/__init__.py
+
+touch config/__init__.py
+touch config/settings.py
+
+touch instance/__init__.py
+touch instance/settings.py
+
+echo "Now generating app files and directories."
+
+touch $name/app.py
+
+react=$2
+
+if [ "${react:-0}" -eq "react" ]; then
+    echo "React template not yet implemented! Will be added soon."
+    exit
+else
+    mkdir templates
+    mkdir static
+    touch templates/layout.html
+    touch static/main.css
+fi
 
 echo "Done!"
